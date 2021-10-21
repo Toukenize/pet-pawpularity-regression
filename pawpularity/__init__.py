30d44d178013beeca8944dbf9efe41ab3bc9acfa
Chew Ze Yong <gavinchew92@gmail.com>
2021-10-21 19:25:37 +0800

    Update __init__ to cater for cases in which no secrets.env is available

--- pawpularity/__init__.py
+++ pawpularity/__init__.py
@@ -3,5 +3,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Assumes config.env is at the same lvl as package root
-load_dotenv(Path(__path__[0]).parent / 'config.env')
-load_dotenv(Path(__path__[0]).parent / 'secrets.env')
+
+_root = Path(__path__[0]).parent
+
+for env_file in ['config.env', 'secrets.env']:
+
+    if (_root / env_file).exists():
+        load_dotenv(_root / env_file)
